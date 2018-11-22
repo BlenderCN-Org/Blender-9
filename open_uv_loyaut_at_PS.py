476688e0228ba7a2260e8564a2ff015fdf24fb63
Alex Andrusevich <aleksey.andrusevich@gmail.com>
2018-11-22 17:56:10 +0200

    Update open_uv_loyaut_at_PS.py

--- open_uv_loyaut_at_PS.py
+++ open_uv_loyaut_at_PS.py
@@ -1,6 +1,14 @@
 import bpy
 
-uvLay ="D:\\uv_layout.png"
-print(uvLay)
-bpy.ops.uv.export_layout(filepath=uvLay, size=(1024, 1024), opacity=0)
-bpy.ops.image.external_edit(filepath=uvLay)
+tempFolder = bpy.app.tempdir
+uvLay =tempFolder+"uv_temp.png"
+
+
+def openInPs():
+	bpy.ops.uv.export_layout(filepath=uvLay, size=(1024, 1024), opacity=0)
+	bpy.ops.image.external_edit(filepath=uvLay)
+	finishOp = True
+
+
+openInPs()
+
